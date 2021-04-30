3b5b136c58e200db01b271894faae33716a65b27
alison <alimcginley@gmail.com>
2021-04-30 12:08:09 -0700

    updates handling of requests

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,30 @@
 from flask import Blueprint
+from app.models.planet import Planet
+from flask import request, Blueprint, make_response, jsonify
 
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route(methods=["GET"], ["POST"])
+def handle_planets():
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id" : planet.id,
+                "name" : planet.name,
+                "description" : planet.description,
+                "distance from earth" : planet.distance_from_earth
+            })
+        return jsonify(planets_response)
+    
+    elif request_method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"],
+                            description=request_body["description"]
+                            distance_from_earth=request_body["distance from earth"]
+                            )
+        db.session.add(new_planet)
+        db.session.commit()
+
+        return make_response(f"Planet {new_planet.name} was successfully added", 201)
